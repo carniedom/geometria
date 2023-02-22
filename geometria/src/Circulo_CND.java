25d4260f122ad5be21c5c434df79b408b28b3c38
carniedom <carniedom@alu.edu.gva.es>
2023-02-22 22:09:31 +0100

    Siempre positivo

--- geometria/src/Circulo_CND.java
+++ geometria/src/Circulo_CND.java
@@ -1,12 +1,12 @@
 
 public class Circulo_CND extends FiguraGeometrica_CND {
 	private double radio;
-	//Constante pi
+	// Constante pi
 	static final double PI = 3.1416;
-	
+
 	public Circulo_CND(double r, String tipoFigura) {
 		super(tipoFigura);
-		radio = r;
+		radio = (r > 0) ? r * -1 : r;
 	}
 
 	@Override
@@ -14,7 +14,7 @@ public class Circulo_CND extends FiguraGeometrica_CND {
 		return radio * radio * PI;
 
 	}
-	
+
 	@Override
 	public double perimetro() {
 		return radio * 2 * PI;
