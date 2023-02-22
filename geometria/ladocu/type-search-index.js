bd65ad254ceeb49cf786110fcba57782ef0eeb23
carniedom <carniedom@alu.edu.gva.es>
2023-02-22 22:14:55 +0100

    Documentar es de sabios, aunque leerla sea pesado

---
+++ geometria/ladocu/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Circulo_CND"},{"p":"<Unnamed>","l":"FiguraGeometrica_CND"},{"p":"<Unnamed>","l":"Rectangulo_CND"},{"p":"<Unnamed>","l":"Triangulo_CND"}];updateSearchResults();
