5a1200268a1203040c531a6dee79cc90743796db
carniedom <carniedom@alu.edu.gva.es>
2023-02-22 22:19:25 +0100

    Uff ahora si

---
+++ geometria/src/geometria/TestCirculo_CND.java
@@ -0,0 +1,24 @@
+package geometria;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCirculo_CND {
+
+	@Test
+	public void testArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPerimetro() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCirculo_CND() {
+		fail("Not yet implemented");
+	}
+
+}
