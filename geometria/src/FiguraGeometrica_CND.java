c91354cf025ada2774c3f1e048189a0e43de7038
carniedom <carniedom@alu.edu.gva.es>
2023-02-22 21:57:21 +0100

    Nuevo método

--- geometria/src/FiguraGeometrica_CND.java
+++ geometria/src/FiguraGeometrica_CND.java
@@ -20,4 +20,7 @@ public abstract class FiguraGeometrica_CND {
 		this.tipoFigura = tipoFigura;
 	}
 
+	public double semiPerimetro() {
+		return this.perimetro() / 2;
+	}
 }
--- geometria/src/Triangulo_CND.java
+++ geometria/src/Triangulo_CND.java
@@ -23,7 +23,4 @@ public class Triangulo_CND extends FiguraGeometrica_CND {
 		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
 	}
 	
-	public double semiPerimetro() {
-		return this.perimetro() / 2;
-	}
 }
